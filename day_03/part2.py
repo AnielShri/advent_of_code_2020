5f94781a87e889050fde037ceab921867f477152
Aniel - Nitro <aniel.shri@outlook.com>
2021-01-25 00:44:33 +0100

    added day 3 - part 2

--- day_03/part2.py
+++ day_03/part2.py
@@ -2,7 +2,7 @@
 
 #-------------------------------------------------------------------+
 #
-# Advent of Code - Day 3 - Part 1
+# Advent of Code - Day 3 - Part 2
 #
 #-------------------------------------------------------------------+
 
@@ -12,6 +12,7 @@
 
 import os
 import re
+import math
 from typing import List
 
 #-------------------------------------------------------------------+
@@ -33,29 +34,48 @@ class Algorithm:
 			return entries
 
 	# core of algorithm
-	def _count_trees(self, entries:List[str]) -> int:
+	def _count_trees(self, entries:List[str], right:int, down:int) -> int:
 		num_trees = 0
 		col = 0
+		row = 0
 		max_col = len(entries[0])
+		max_row = len(entries)
 
-		for row in entries:
-			if row[col] == "#":
+		# we're not stepping through each entry, so use an infinite loop, and break at end of list
+		while True:
+			item = entries[row]
+			if item[col] == "#":
 				num_trees = num_trees + 1
 			#end if 
 
-			# calculate next index, account for overflow -> repeating biome pattern
-			col = (col + 3) %  max_col 
-		#end for
-		
+			# calculate next col, account for overflow -> repeating biome pattern
+			col = (col + right) %  max_col
+			
+			# move down until end last entry
+			row = row + down
+			if row >= max_row:
+				break
+		#end loop
+
 		return num_trees		
 
 	# public function 
 	def execute(self, filename:str) -> int:
 		entries = self._load_data(filename)
 
-		num_trees = self._count_trees(entries)
+		slopes = [
+			[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]
+		]
 
-		return num_trees
+		num_trees = [0] * len(slopes)
+		
+		for n in range(len(slopes)):
+			num_trees[n] += self._count_trees(entries, slopes[n][0], slopes[n][1])
+		#end for
+
+		prod_trees = math.prod(num_trees)
+
+		return prod_trees
 		
 
 #-------------------------------------------------------------------+
