c2785d7a2497dcdff05a7a556cb8c2baf434ed27
Aniel - Nitro <aniel.shri@outlook.com>
2021-01-24 04:46:28 +0100

    Added day 2

--- day_02/part1.py
+++ day_02/part1.py
@@ -2,7 +2,7 @@
 
 #-------------------------------------------------------------------+
 #
-# Advent of Code - Day 1 - Part 1
+# Advent of Code - Day 2 - Part 1
 #
 #-------------------------------------------------------------------+
 
@@ -11,6 +11,8 @@
 #-------------------------------------------------------------------+
 
 import os
+import re
+from typing import List
 
 #-------------------------------------------------------------------+
 #	main algorithm
@@ -20,33 +22,48 @@ class Algorithm:
 	def __init__(self):
 		pass
 
-	def execute(self, file: str) -> int:
+	# load input
+	def _load_data(self, filename:str) -> List[str]:
 		try:
-			entries = [int(data.rstrip()) for data in open(file)]
-
-			found = False
-			prod_lines = 0
-			for x in entries:
-				for y in entries:
-					sum_lines = x + y
-					if sum_lines == 2020:
-						prod_lines = x * y
-						found = True
-						print("{} + {} = 2020".format(x, y))
-						break
-					#endif
-				#endfor
-				
-				if found == True:
-					break
-				#endif
-			#endfor
-
+			entries = [data.rstrip() for data in open(filename)]
 		except Exception as e:
 			print("Exception: {}".format(e))
-			return -1
+			return [""]
 		else:
-			return prod_lines
+			return entries
+
+	# core of algorithm
+	def _valid_entries(self, entries:List[str]) -> int:
+		# regular expression works on string format as found in input text
+		regex = re.compile("(?P<min>\d+)-(?P<max>\d+) (?P<letter>\w): (?P<pwd>\w+)")
+
+
+		num_valid = 0
+
+		for item in entries:
+			parts = regex.match(item)
+
+			count = 0
+			for n in parts.group("pwd"):
+				if n == parts.group("letter"):
+					count = count + 1
+			#end for
+
+			if count >= int(parts.group("min")) and count <= int(parts.group("max")):
+				num_valid = num_valid + 1
+				
+		#end for
+
+		return num_valid		
+
+	# public function 
+	def execute(self, filename:str) -> int:
+		entries = self._load_data(filename)
+
+		valid_entries = self._valid_entries(entries)
+
+		return valid_entries
+		
 
 #-------------------------------------------------------------------+
 #	startup
---
+++ day_02/part2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#-------------------------------------------------------------------+
+#
+# Advent of Code - Day 2 - Part 2
+#
+#-------------------------------------------------------------------+
+
+#-------------------------------------------------------------------+
+#	dependencies
+#-------------------------------------------------------------------+
+
+import os
+import re
+from typing import List
+
+#-------------------------------------------------------------------+
+#	main algorithm
+#-------------------------------------------------------------------+
+
+class Algorithm:
+	def __init__(self):
+		pass
+
+	# load input
+	def _load_data(self, filename:str) -> List[str]:
+		try:
+			entries = [data.rstrip() for data in open(filename)]
+		except Exception as e:
+			print("Exception: {}".format(e))
+			return [""]
+		else:
+			return entries
+
+	# core of algorithm
+	def _valid_entries(self, entries:List[str]) -> int:
+		# regular expression works on string format as found in input text
+		regex = re.compile("(?P<yes>\d+)-(?P<no>\d+) (?P<letter>\w): (?P<pwd>\w+)")
+
+
+		num_valid = 0
+
+		for item in entries:
+			parts = regex.match(item)
+
+			index_yes = int(parts.group("yes")) - 1
+			index_no = int(parts.group("no")) - 1
+			pwd = parts.group("pwd")
+
+			has_yes = False
+			has_no = False
+
+			if pwd[index_yes] == parts.group("letter"):
+				has_yes = True
+			
+			if pwd[index_no] == parts.group("letter"):
+				has_no = True
+
+			if has_yes == True and has_no == False:
+				num_valid = num_valid + 1
+
+			if has_yes == False and has_no == True:
+				num_valid = num_valid + 1
+		#end for
+		
+		return num_valid		
+
+	# public function 
+	def execute(self, filename:str) -> int:
+		entries = self._load_data(filename)
+
+		valid_entries = self._valid_entries(entries)
+
+		return valid_entries
+		
+
+#-------------------------------------------------------------------+
+#	startup
+#-------------------------------------------------------------------+
+if __name__ == "__main__":
+	separator = "\r\n+----------------------------+\r\n"
+	filename = "{}/input.txt".format(os.path.dirname(__file__))
+
+	print(separator)
+
+	alg = Algorithm()
+	data = alg.execute(filename)
+	print("\nProduct total: {}".format(data))
+
+	print(separator)
